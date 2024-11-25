3ce024c60165361d5afdb359badf2b6b25c3eab4
Tuncay Arda Aydın <tuaydin@k1m01s01.42istanbul.com.tr>
2024-11-25 19:41:35 +0300

    signal

--- srcs/client.c
+++ srcs/client.c
@@ -6,15 +6,56 @@
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 20:27:54 by tuaydin           #+#    #+#             */
-/*   Updated: 2024/11/24 21:02:43 by tuaydin          ###   ########.fr       */
+/*   Updated: 2024/11/25 18:39:03 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 #include <signal.h>
+#include <stdio.h>
+#define POSIX_C_SOURCE 208009L
+
+int	status;
+
+void	confirm(int signal)
+{
+	if (signal == SIGUSR1)
+		status = 0;
+}
+
+void	send_char(int pid, char c)
+{
+	int		shift;
+
+	shift = 7;
+	while (shift >= 0)
+	{
+		status = 1;
+		if ((c >> shift & 1) == 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		while (status)
+			pause();
+		usleep(2);
+		status = 1;
+		shift--;
+	}
+}
 
 int main(int an, char* args[])
 {
-	kill(atoi(args[1]), SIGUSR1);
+	int	i;
+	int	pid;
+
+	i = 0;
+	pid = ft_atoi(args[1]);
+	signal(SIGUSR1, confirm);
+	while (args[2][i])
+	{
+		send_char(pid, args[2][i]);
+		i++;
+	}
+	
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,25 +6,56 @@
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 20:27:50 by tuaydin           #+#    #+#             */
-/*   Updated: 2024/11/24 21:15:43 by tuaydin          ###   ########.fr       */
+/*   Updated: 2024/11/25 18:28:23 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "unistd.h"
 #include "ft_printf.h"
 #include "libft.h"
 #include <signal.h>
+#include <sys/types.h>
+#include "server.h"
+#define POSIX_C_SOURCE 208009L
 
-void	handler()
+
+t_bits bits;
+
+void	handle(int  signal,siginfo_t *info,void *context)
 {
-	ft_printf("test\n");
+
+	(void)context;
+	
+	bits.c = bits.c << 1;
+	if (signal == SIGUSR1)
+		bits.c = bits.c | 1;
+	else if (signal == SIGUSR2)
+		bits.c = bits.c | 0;
+	kill(info->si_pid, SIGUSR1);
+	bits.count++;
+	if (bits.count == 8)
+	{
+		ft_printf("%c", bits.c);
+		bits.count = 0;
+		bits.c = 0;
+	}
+		
 }
 
 int main()
 {
-	signal(SIGUSR1, handler);
+	struct sigaction a;
+	
+	a.sa_sigaction = handle;
+	a.sa_flags= SA_SIGINFO;
+	
+	bits.c = 0;
+	bits.count = 0;
+	sigaction(SIGUSR1,&a,NULL);
+	sigaction(SIGUSR2,&a,NULL);
 	ft_printf("PID:%d\n",getpid());
 	while(1)
 	{
-		sleep(1);
+		pause();
 	}
 }
